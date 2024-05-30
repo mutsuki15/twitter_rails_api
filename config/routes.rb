ed758df51a9adc12672d67521f1e0ef43bbcf4db
mutsuki15 <straw.lznuscrfb.0105@gmail.com>
2024-05-30 23:33:22 +0900

    deviseとdevise_token_authのインストール #1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth'
   resources :tasks
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
