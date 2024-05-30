dcd3f33a44b63f9402b056fad675403777e75579
mutsuki15 <straw.lznuscrfb.0105@gmail.com>
2024-05-30 23:23:10 +0900

    letter_openerの設定 #1

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@
 
 Rails.application.routes.draw do
   resources :tasks
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
