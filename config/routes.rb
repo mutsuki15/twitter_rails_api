08257848e23e3c539939db3845a76a8a25932aaa
mutsuki15 <straw.lznuscrfb.0105@gmail.com>
2024-05-31 00:37:22 +0900

    Userモデル/コントローラ/ルーティングの作成 #1

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
+  namespace :api do
+    namespace :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+        registrations: 'api/v1/auth/registrations'
+      }
+    end
+  end
   resources :tasks
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
 end
