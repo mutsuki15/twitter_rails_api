d3b1c642b0f451cba91de1224252b47160d150cb
mutsuki15 <straw.lznuscrfb.0105@gmail.com>
2024-06-26 00:45:10 +0900

    dbの修正 #1

--- db/schema.rb
+++ db/schema.rb
@@ -43,6 +43,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_30_143015) do
     t.datetime "updated_at", null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
