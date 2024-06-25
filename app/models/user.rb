295feff571519bafc0bde66655180e111f9c5c14
mutsuki15 <straw.lznuscrfb.0105@gmail.com>
2024-06-26 00:46:13 +0900

    model/controllerの修正 #1

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,10 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable
+  
+  validates :name, presence: true, length: { maximum: 16 }, uniqueness: true
+  validates :phone, presence: true
+  validates :birthday, presence: true
+
   include DeviseTokenAuth::Concerns::User
 end
