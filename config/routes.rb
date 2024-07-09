51f832351d4752ba6c47b075ccde7090052c137c
mutsuki15 <straw.lznuscrfb.0105@gmail.com>
2024-07-09 23:19:25 +0900

    user modelとuser controllerの作成 #12

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
       mount_devise_token_auth_for 'User', at: 'users', controllers: {
         registrations: 'api/v1/users/registrations'
       }
+      resources :users, param: :name, only: %i[show update]
     end
   end
   resources :tasks
