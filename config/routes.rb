9bd08c6871746b815f306b3731fa871c92ad3114
mutsuki15 <straw.lznuscrfb.0105@gmail.com>
2024-07-10 00:25:41 +0900

    tweets controllerに削除機能を追加 #14

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-    resources :tweets, only: %i[index show create]
+    resources :tweets, only: %i[index show create destroy]
     resources :images, only: %i[update]
       mount_devise_token_auth_for 'User', at: 'users', controllers: {
         registrations: 'api/v1/users/registrations'
